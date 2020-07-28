cdcee8fffd52b73744be72ad3230e6237bc7be4c
GOL-DOLM <61331138+GOL-DOLM@users.noreply.github.com>
2020-07-28 09:46:29 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -58,3 +58,24 @@ $(window).on('load', function() {
 
 })(jQuery);
 
+// this one is jut to wait for the page to load
+document.addEventListener('DOMContentLoaded', () => {
+
+    const themeStylesheet = document.getElementById('theme');
+    const storedTheme = localStorage.getItem('theme');
+    if(storedTheme){
+        themeStylesheet.href = storedTheme;
+    }
+    const themeToggle = document.getElementById('theme-toggle');
+    themeToggle.addEventListener('click', () => {
+        // if it's light -> go dark
+        if(themeStylesheet.href.includes('light')){
+            themeStylesheet.href = 'css/dark-theme.css';
+        } else {
+            // if it's dark -> go light
+            themeStylesheet.href = 'css/light-theme.css';
+        }
+        // save the preference to localStorage
+        localStorage.setItem('theme',themeStylesheet.href)  
+    });
+});
